50be097b512c652a25b1e0405037668e45ffac22
Atul Kesharwani <60789960+Atu77l@users.noreply.github.com>
2022-01-20 22:46:57 +0530

    Update News.js

--- src/News.js
+++ src/News.js
@@ -24,7 +24,7 @@ class News extends Component {
     let { articles, source } = this.state
     const { classes } = this.props
 
-    if (source != this.props.source) this.reloadSource()
+    if (source !== this.props.source) this.reloadSource()
     return (
       <div className={classes.root}>
         <GridList cellHeight={180} className={classes.gridList}>
