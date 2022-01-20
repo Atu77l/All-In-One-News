2128986c6819413ed4cdf6d5d2aaa21d7e6537b8
Atul Kesharwani <kesharwaniatul9935@gmail.com>
2022-01-20 21:20:20 +0530

    file upload

---
+++ src/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  topHeadlinesURL: 'https://newsapi.org/v2/top-headlines?',
+  sourcesURL: 'https://newsapi.org/v2/sources?',
+  everythingURL: 'https://newsapi.org/v2/everything?',
+  countryID: 'country=id&',
+  apiKey: 'apiKey=558ac6d1fecc4e5a8664f2f2e8761713',
+  sources: '&sources=',
+  search: '&q='
+}
