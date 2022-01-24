6c2c94ae1fa7a09f1545f484d45ec12bcb9883d6
Atul Kesharwani <kesharwaniatul9935@gmail.com>
2022-01-24 10:27:07 +0530

    Updates

---
+++ precache-manifest.e39c21aa55db5e17b06c5a3ad249d4ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a96cf3ac5ea5983eb48d1cdd78e1a927",
+    "url": "/All-In-One-News/index.html"
+  },
+  {
+    "revision": "0cda052af6d8409f6d0e",
+    "url": "/All-In-One-News/static/css/main.fdeec0e2.chunk.css"
+  },
+  {
+    "revision": "ee9ce1b58be21c186ebe",
+    "url": "/All-In-One-News/static/js/2.9177a55e.chunk.js"
+  },
+  {
+    "revision": "0cda052af6d8409f6d0e",
+    "url": "/All-In-One-News/static/js/main.92b08d9a.chunk.js"
+  },
+  {
+    "revision": "82f3a79f26f943634d48",
+    "url": "/All-In-One-News/static/js/runtime~main.e359c2ab.js"
+  },
+  {
+    "revision": "3acf4071dae5f2f108e155eaeeb41faf",
+    "url": "/All-In-One-News/static/media/newspaper.3acf4071.jpg"
+  }
+]);
